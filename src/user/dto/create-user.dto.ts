08d7ec96b4f3cdefd4654cdd337d0f3061c89c63
Matheus Magno <matheusmagnon@gmail.com>
2024-02-10 17:38:33 -0300

    chore: create DTO to routers

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsNotEmpty, IsString, IsStrongPassword } from "class-validator";
+
+export class CreateUserDTO {
+
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsStrongPassword({
+        minLength: 6,
+        minNumbers: 0,
+        minLowercase: 0,
+        minSymbols: 0,
+        minUppercase: 0
+    })
+    password: string;
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,13 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put } from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdateUSerDTO } from './dto/update-put-user.dto';
+import { UpdatePatchUSerDTO } from './dto/update-patch-user.dto';
 
 @Controller('users')
 export class UserController {
   @Post()
-  async create(@Body() body) {
-    return { body };
+  async create(@Body() { email, name, password }: CreateUserDTO) {
+    return { email, name, password };
   }
 
   @Get()
@@ -13,33 +16,33 @@ export class UserController {
   }
 
   @Get(':id')
-  async showId(@Param() params) {
-    return { user: {}, params }
+  async showId(@Param('id', ParseIntPipe) id: number) {
+    return { user: {}, id }
   }
 
   @Put(':id')
-  async update(@Body() body, @Param() param) {
+  async update(@Body() { email, name, password }: UpdateUSerDTO, @Param('id', ParseIntPipe) id: number) {
     return {
       method: 'put',
-      body,
-      param
+      email, name, password,
+      id
     }
   }
 
 
   @Patch(':id')
-  async updatePartial(@Body() body, @Param() param) {
+  async updatePartial(@Body() { email, name, password }: UpdatePatchUSerDTO, @Param('id', ParseIntPipe) id: number) {
     return {
       method: 'path',
-      body,
-      param
+      email, name, password,
+      id
     }
   }
 
   @Delete(':id')
-  async delete(@Param() param) {
+  async delete(@Param('id', ParseIntPipe) id: number) {
     return {
-      param
+      id
     }
   }
 
