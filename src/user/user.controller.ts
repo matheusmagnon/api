e6f27f493d0b47678ab3b9bdc700f8c36767dace
Matheus Magno <matheusmagnon@gmail.com>
2024-02-12 17:55:15 -0300

    configure db w/ prisma

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,49 +1,47 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
-import { UpdateUSerDTO } from './dto/update-put-user.dto';
+import { UpdatePutUSerDTO } from './dto/update-put-user.dto';
 import { UpdatePatchUSerDTO } from './dto/update-patch-user.dto';
+import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
+
+  constructor(private readonly userService: UserService) { }
+
   @Post()
-  async create(@Body() { email, name, password }: CreateUserDTO) {
-    return { email, name, password };
+  async create(@Body() { email, name, password, birthAt }: CreateUserDTO) {
+    return this.userService.create({ email, name, password, birthAt });
   }
 
   @Get()
   async list() {
-    return { users: [] }
+    return this.userService.list()
   }
 
   @Get(':id')
   async showId(@Param('id', ParseIntPipe) id: number) {
-    return { user: {}, id }
+    return this.userService.showId(id)
   }
 
   @Put(':id')
-  async update(@Body() { email, name, password }: UpdateUSerDTO, @Param('id', ParseIntPipe) id: number) {
-    return {
-      method: 'put',
-      email, name, password,
-      id
-    }
+  async update(@Body() data: UpdatePutUSerDTO, @Param('id', ParseIntPipe) id: number) {
+    console.log(data.birthAt)
+
+    return this.userService.update(id, data)
   }
 
 
   @Patch(':id')
-  async updatePartial(@Body() { email, name, password }: UpdatePatchUSerDTO, @Param('id', ParseIntPipe) id: number) {
-    return {
-      method: 'path',
-      email, name, password,
-      id
-    }
+  async updatePartial(@Body() data: UpdatePatchUSerDTO,
+    @Param('id', ParseIntPipe) id: number) {
+    return this.userService.updatePartial(id, data);
+
   }
 
   @Delete(':id')
   async delete(@Param('id', ParseIntPipe) id: number) {
-    return {
-      id
-    }
+    return this.userService.delete(id)
   }
 
 }
---
+++ src/user/user.service.ts
@@ -0,0 +1,95 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { CreateUserDTO } from "./dto/create-user.dto";
+import { PrismaService } from "src/prisma/prisma.service";
+import { UpdatePutUSerDTO } from "./dto/update-put-user.dto";
+import { UpdatePatchUSerDTO } from "./dto/update-patch-user.dto";
+
+@Injectable()
+export class UserService {
+
+    constructor(private readonly prisma: PrismaService) { }
+    async create({ email, name, password, birthAt }: CreateUserDTO) {
+        return this.prisma.user.create({
+            data: {
+                email,
+                name,
+                password,
+                birthAt: birthAt ? new Date(birthAt) : null,
+            }
+        })
+    }
+
+    async list() {
+        return this.prisma.user.findMany()
+    }
+
+    async showId(id: number) {
+
+        return this.prisma.user.findUnique({
+            where: {
+                id
+            }
+        })
+    }
+
+    async update(id: number, { birthAt, email, name, password }: UpdatePutUSerDTO) {
+
+        this.exists(id)
+
+
+        if (!birthAt) {
+            birthAt = null;
+        }
+
+
+        return this.prisma.user.update({
+            data: { email, name, password, birthAt: birthAt ? new Date(birthAt) : null },
+            where: {
+                id
+            }
+        })
+    }
+
+    async updatePartial(id: number, { birthAt, email, name, password }: UpdatePatchUSerDTO) {
+
+        this.exists(id)
+
+        const data: any = {}
+
+        if (birthAt) {
+            data.birthAt = new Date(birthAt)
+        }
+        if (email) {
+            data.email = email
+        }
+        if (name) {
+            data.name = name
+        }
+        if (password) {
+            data.password = password
+        }
+
+        return this.prisma.user.update({
+            data,
+            where: {
+                id
+            }
+        })
+    }
+
+    async delete(id: number) {
+
+        this.exists(id)
+        return this.prisma.user.delete({
+            where: {
+                id
+            }
+        })
+    }
+
+    async exists(id: number) {
+        if (!(await this.showId(id))) {
+            throw new NotFoundException(`O usuário ${id} não existe!`)
+        }
+    }
+}
