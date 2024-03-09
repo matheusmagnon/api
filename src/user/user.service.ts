91d98c1634f03ada8b771d9b3a4d7fa9e31be40a
Matheus Magno <matheusmagnon@gmail.com>
2024-03-09 15:49:42 -0300

    feat: improviment service function to validate with user id exist

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,91 +6,102 @@ import { UpdatePatchUSerDTO } from "./dto/update-patch-user.dto";
 
 @Injectable()
 export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+  async create({ email, name, password, birthAt }: CreateUserDTO) {
+    return this.prisma.user.create({
+      data: {
+        email,
+        name,
+        password,
+        birthAt: birthAt ? new Date(birthAt) : null,
+      },
+    });
+  }
 
-    constructor(private readonly prisma: PrismaService) { }
-    async create({ email, name, password, birthAt }: CreateUserDTO) {
-        return this.prisma.user.create({
-            data: {
-                email,
-                name,
-                password,
-                birthAt: birthAt ? new Date(birthAt) : null,
-            }
-        })
+  async list() {
+    return this.prisma.user.findMany();
+  }
+
+  async showId(id: number) {
+    await this.exists(id);
+
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(
+    id: number,
+    { birthAt, email, name, password }: UpdatePutUSerDTO
+  ) {
+    await this.exists(id);
+
+    if (!birthAt) {
+      birthAt = null;
     }
 
-    async list() {
-        return this.prisma.user.findMany()
+    return this.prisma.user.update({
+      data: {
+        email,
+        name,
+        password,
+        birthAt: birthAt ? new Date(birthAt) : null,
+      },
+      where: {
+        id,
+      },
+    });
+  }
+
+  async updatePartial(
+    id: number,
+    { birthAt, email, name, password }: UpdatePatchUSerDTO
+  ) {
+    await this.exists(id);
+
+    const data: any = {};
+
+    if (birthAt) {
+      data.birthAt = new Date(birthAt);
+    }
+    if (email) {
+      data.email = email;
+    }
+    if (name) {
+      data.name = name;
+    }
+    if (password) {
+      data.password = password;
     }
 
-    async showId(id: number) {
+    return this.prisma.user.update({
+      data,
+      where: {
+        id,
+      },
+    });
+  }
 
-        return this.prisma.user.findUnique({
-            where: {
-                id
-            }
-        })
+  async delete(id: number) {
+    await this.exists(id);
+    return this.prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async exists(id: number) {
+    if (
+      !(await this.prisma.user.count({
+        where: {
+          id,
+        },
+      }))
+    ) {
+      throw new NotFoundException(`O usuário ${id} não existe!`);
     }
-
-    async update(id: number, { birthAt, email, name, password }: UpdatePutUSerDTO) {
-
-        await this.exists(id)
-
-
-        if (!birthAt) {
-            birthAt = null;
-        }
-
-
-        return this.prisma.user.update({
-            data: { email, name, password, birthAt: birthAt ? new Date(birthAt) : null },
-            where: {
-                id
-            }
-        })
-    }
-
-    async updatePartial(id: number, { birthAt, email, name, password }: UpdatePatchUSerDTO) {
-
-        await this.exists(id)
-
-
-        const data: any = {}
-
-        if (birthAt) {
-            data.birthAt = new Date(birthAt)
-        }
-        if (email) {
-            data.email = email
-        }
-        if (name) {
-            data.name = name
-        }
-        if (password) {
-            data.password = password
-        }
-
-        return this.prisma.user.update({
-            data,
-            where: {
-                id
-            }
-        })
-    }
-
-    async delete(id: number) {
-
-        await this.exists(id)
-        return this.prisma.user.delete({
-            where: {
-                id
-            }
-        })
-    }
-
-    async exists(id: number) {
-        if (!(await this.showId(id))) {
-            throw new NotFoundException(`O usuário ${id} não existe!`)
-        }
-    }
+  }
 }
